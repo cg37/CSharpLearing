09f49b0b198e48c18c0df7e99c1e87894b049b4e
Craig <cg37@outlook.com>
2026-04-24 19:55:42 +0800

    feat: 一个打印99乘法表的代码练习

---
+++ CSharpLearning/CSharpLearning/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace  CSharpLearning;
+
+internal class Program
+{
+    static void Main()
+    {
+        /*
+         *一个打印99乘法表的代码片段
+         */
+        for (short i = 1; i < 10; ++i)
+        {
+            for (short j = 1; j <= i; ++j)
+            {
+                Console.Write($"{j, 3} * {i} = {i * j};");
+            }
+            Console.WriteLine();
+        }
+    }
+}
